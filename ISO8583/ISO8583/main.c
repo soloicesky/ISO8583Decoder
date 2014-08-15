1369ca141a5491137ca229cb222f3a17abf31acc
soloicesky <soloicesky@Soloiceskys-MacBook-Pro.local>
2014-08-15 10:31:22 +0800

    add up Xcode project

---
+++ ISO8583/ISO8583/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  ISO8583
+//
+//  Created by Soloicesky on 8/15/14.
+//  Copyright (c) 2014 Soloicesky. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
